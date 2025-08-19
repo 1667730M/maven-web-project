ff61c6459eafa99f7c08dd9ec0a2fc711111671a
1667730M <powerblast2015@gmail.com>
2025-08-20 01:27:54 +0800

    test: add test case for register servlet

--- src/main/test/first/webapp/RegisterServletTest.java
+++
@@ -1,5 +0,0 @@
-package first.webapp;
-
-//public class RegisterServletTest {
-//
-//}
---
+++ src/test/java/first/webapp/RegisterServletTest.java
@@ -0,0 +1,73 @@
+package first.webapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+ 
+import org.junit.jupiter.api.Test;
+ 
+import static org.mockito.Mockito.*;
+import org.mockito.MockedStatic;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+
+public class RegisterServletTest {
+	private RegisterServlet servlet;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private Connection mockConnection;
+	private PreparedStatement mockPreparedStatement;
+	 
+	@BeforeEach
+	private void setUp() {
+	   servlet = new RegisterServlet();
+	   request = mock(HttpServletRequest.class);
+	   response = mock(HttpServletResponse.class);
+	   mockConnection = mock(Connection.class);
+	   mockPreparedStatement = mock(PreparedStatement.class);
+	}
+	
+	@Test
+	void testDoPostSuccess() throws Exception {
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter writer = new PrintWriter(stringWriter);
+		when(response.getWriter()).thenReturn(writer);
+		
+		when(request.getParameter("userName")).thenReturn("eric");
+		when(request.getParameter("userName")).thenReturn("password123");
+		when(request.getParameter("userName")).thenReturn("eric@gmail.com");
+		when(request.getParameter("userName")).thenReturn("English");
+		
+		MockedStatic<DriverManager> mockedDriverManager = mockStatic(DriverManager.class);
+		mockedDriverManager.when(() -> DriverManager.getConnection(anyString(), anyString(), anyString())).thenReturn(mockConnection);
+		
+		when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+		when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+		
+		servlet.doPost(request, response);
+		
+		writer.flush();
+		String output = stringWriter.toString();
+		assertTrue(output.contains("You are successfully registered"));
+	
+		verify(mockPreparedStatement).setString(1, "eric");
+		verify(mockPreparedStatement).setString(2, "password123");
+		verify(mockPreparedStatement).setString(2, "eric@gmail.com");
+		verify(mockPreparedStatement).setString(2, "English");
+	}
+
+}
+
+
+ 
+
+ 
